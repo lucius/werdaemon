4b50cb7453b1f74573084622ab4fd19028fc11e2
herrera.rp <herrera.rp@f5a38e73-9655-0410-af59-dffacbec67f1>
2008-09-01 09:21:10 +0000

    incluidas bibliotecas para transmissao de dados via socket, os dados devem ser mapeados em strings e transmitidos, o recebimento funciona atraves do o processo inverso...   sera necessario fazer o mapeamento na proxima etapa...

---
+++ trunk/werd/exemploCliente.cpp
@@ -0,0 +1,39 @@
+#include "ClientSocket.h"
+#include "SocketException.h"
+#include <iostream>
+#include <string>
+
+int main ( int argc, int argv[] )
+{
+    try
+    {
+        ClientSocket client_socket ( "localhost", 6666 );
+
+        std::string
+        resposta;
+
+        try
+        {
+            client_socket << "INICIO";
+            client_socket >> resposta;
+        }
+        catch ( SocketException& e )
+        {
+            std::cout << "Excessao lancada: " << e.description() << std::endl;
+        }
+
+        std::cout << "Resposta recebida do servidor servidor: " << resposta << std::endl;
+
+        if (resposta == "OK")
+        {
+            std::cout << "Protocolo aceito... Enviando dados da operacao..." << std::endl;
+            client_socket << "DADOS_OPERACAO";
+        }
+    }
+    catch ( SocketException& e )
+    {
+        std::cout << "Excessao lancada: " << e.description() << std::endl;
+    }
+
+  return 0;
+}
