bf9f9f4d0008736055f19268ba491bf040380a93
herrera.rp <herrera.rp@f5a38e73-9655-0410-af59-dffacbec67f1>
2008-09-01 09:21:10 +0000

    incluidas bibliotecas para transmissao de dados via socket, os dados devem ser mapeados em strings e transmitidos, o recebimento funciona atraves do o processo inverso...   sera necessario fazer o mapeamento na proxima etapa...

---
+++ werd/exemploServidor.cpp
@@ -0,0 +1,52 @@
+#include "ServerSocket.h"
+#include "SocketException.h"
+#include <iostream>
+#include <string>
+
+int main ( int argc, int argv[] )
+{
+    std::cout << "Rodando..." << std::endl;
+
+    try
+    {
+        ServerSocket server ( 6666 );
+
+        while ( true )
+        {
+            ServerSocket new_sock;
+            server.accept ( new_sock );
+
+            try
+            {
+                std::string
+                data;
+
+                while ( true )
+                {
+                    std::string
+                    data;
+
+                    new_sock >> data;
+
+                    if (data == "INICIO")
+                    {
+                        new_sock << "OK";
+                        new_sock >> data;
+                        std::cout << "data: " << data << std::endl;
+                    }
+                }
+
+            }
+            catch ( SocketException& )
+            {
+
+            }
+        }
+    }
+    catch ( SocketException& e )
+    {
+        std::cout << "Excessao lancada: " << e.description() << std::endl;
+    }
+
+    return 0;
+}
